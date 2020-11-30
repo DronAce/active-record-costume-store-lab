9dc6f22c208ae675a2a98771f800fb5fbcd2d1b5
DronAce <okeynazone@gmail.com>
2020-11-30 13:22:16 -0500

    commit tree a64076c93cf798de2ec962540e4b0cb83ec8cfd1

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,3 +9,16 @@
 # value types according to the spec
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+
+class CreateCostumes < ActiveRecord::Migration[4.2]
+    def change
+        create_table :costumes do |column|
+            column.string   :name
+            column.float  :price
+            column.string   :size
+            column.string   :image_url
+
+            column.timestamps null: false
+        end
+    end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,19 @@
 # Create your haunted_houses migration here
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+    def change
+        create_table :haunted_houses do |column|
+            column.string   :name
+            column.text     :location
+            column.string   :theme
+            column.float  :price
+            column.boolean  :family_friendly
+            column.datetime :opening_date
+            column.datetime :closing_date
+            column.text     :description
+
+            column.timestamps null: false
+        end
+    end
+end
